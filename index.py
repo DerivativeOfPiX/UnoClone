9b08cd94e5854a0e9fdaa2fea26f1fe4c941e102
DerivativeOfPiX <ma06762@st.habib.edu.pk>
2020-10-26 14:07:36 +0500

    adding turn handlers, card ids, added utility codebase

--- index.py
+++ index.py
@@ -1,42 +1,21 @@
-import random
+from uno_utility import *
 
-deck_color = ["red", "green", "blue", "yellow"]
-deck_action = ["draw_two", "skip", "reverse"]
-deck_special = ["draw_four", "wild"]
-#should add a demiliting string in b/w to seperate out the card name
-delimit_char = '|'
-final_deck = []
+deck = generate_deck()
 
-def generate_deck():
-    #generating cards 1-9
-    for i in range(4):
-        for j in range(9):
-            final_deck.append( str(j + 1) + delimit_char + deck_color[i])
-    for i in range(4):
-        for j in range(3):
-            for k in range(2):
-                final_deck.append(deck_action[j] + delimit_char + deck_color[i])
-    random.shuffle(final_deck)
-    #print(final_deck)
-        
 total_players = 0
-player_decks = [] #multidimensional arrays.
-starting_hard = 8
+player_decks = [] #multidimensional array.
+starting_hard = 8#how many cards each player gets
 stack = [] #this variable stores each card played
 
 def initialize_game():
     start = 0
     end = starting_hard
     for i in range(total_players):
-            player_decks.append(final_deck[start:end])
-            start += starting_hard
-            end += starting_hard 
-    #print(player_decks)
-
-def validate_card(card):
-    if(card == stack[-1]):
-    #check if the card matches the color or number of last card played
-    #can't use object so have to make do with strings :/
+            player_decks.append(deck[start:end])
+            del deck[start:end]
+    stack.append(deck[-1])
+    print(stack)
+    #print(player_decks)    
 
 
 def check_end_condition():
@@ -52,26 +31,28 @@ def turn_handler(current_turn):
     print('Player {0}'.format(current_turn + 1))
     print('Deck : {0}'.format(current_deck))
     #get input
-    current_card_id = int(input("Select a Card to Play: "))
-    print(current_deck[current_card_id - 1])#making sure it's easier for accessiblity purposes
-    validate_card(current_deck[current_card_id])
+    current_card_id = int(input("Select a Card to Play: ")) - 1
+    print(current_deck[current_card_id])#making sure it's easier for accessiblity purposes
+    if(validate_card(current_deck[current_card_id], stack)):#should give an error for greater than 0 then
+        stack.append(current_deck[current_card_id])
+        print('Current Stack :', stack[-1])
+        return
+    else:
+        print("Invalid Card, please try again !")
+        turn_handler(current_turn)#not sure if it is okay to call the function inside the function ?
     #lay it on stack
 
     
 
 def game_loop():
     player_turn = 0
-    turn_handler(player_turn)
     while(check_end_condition()):
-        
-        if(player_turn == 2):
+        if(player_turn == total_players):
             player_turn == 0
         else:
+            turn_handler(player_turn)
             player_turn += 1
             
-
-    
-
 
 total_players = int(input("Enter Number of Players : ")) - 1 #easier to do index calculations this way
 generate_deck()
